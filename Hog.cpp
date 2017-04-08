9c4a3012c1aa1a643b44d2eb23f2a19bf845f474
molasia <molasia@indiana.edu>
2017-04-08 18:11:39 -0400

    Hog skeleton file

---
+++ Hog.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv )
+{
+    // Read an image
+    Mat img = imread("bengal-tiger-why-matter_7341043.jpg");
+    int rows = img.rows;
+    int cols = img.cols;
+    
+    // Crop out a patch
+    
+
+    // Subsample images to size 64*128
+    int width = 64;
+    int height = 128;
+    Mat resizedImg;
+    resize(img, resizedImg, Size(width,height));
+    //cout<< resizedImg.at<double>(10,10)<<endl;
+    //cout<<resizedImg.rows<<endl;
+    //img.convertTo(img, CV_32F, 1/255.0);
+ 
+   // Calculate gradients gx, gy
+   //Mat gx, gy; 
+   //Sobel(img, gx, CV_32F, 1, 0, 1);
+   // Sobel(img, gy, CV_32F, 0, 1, 1);
+   // Mat mag, angle; 
+   // cartToPolar(gx, gy, mag, angle, 1); 
+
+   // cout<< mag.at<double>(10,10)<<endl;
+    return 0;
+}
